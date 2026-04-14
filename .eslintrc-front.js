e897a8faf79bdda12c7d430d07e9d0e136f79cd9
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-04-14 23:10:25 +0200

    flat config eslint

--- .eslintrc-front.js
+++ .eslintrc-front.js
@@ -1,3 +1,17 @@
-module.exports = {
-    "extends": [ "plugin:eslint-plugin-personnallinter/react" ]
-};
+// deps
+
+    // externals
+    const { defineConfig, globalIgnores } = require("eslint/config");
+    const personnallinter = require("eslint-plugin-personnallinter");
+
+// module
+
+module.exports = defineConfig([
+    {
+        "plugins": {
+            personnallinter
+        },
+        "extends": [ personnallinter.configs["react"] ]
+    },
+    globalIgnores([ "public/src/descriptor.ts" ])
+]);
