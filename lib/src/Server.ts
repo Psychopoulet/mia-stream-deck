3a61da0e5346f5e1bcbca2a94da17f3b2e5e63d1
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-05-18 21:34:26 +0200

    event bind corrections

--- lib/src/Server.ts
+++ lib/src/Server.ts
@@ -5,9 +5,6 @@
 
 // types & interfaces
 
-    // externals
-    import type { iMediatorUserOptions } from "node-pluginsmanager-plugin";
-
     // locals
     import type { components } from "./Descriptor";
     import type MediatorStreamDeck from "./Mediator";
@@ -15,16 +12,6 @@
 // module
 
 export default class ServerStreamDeck extends Server {
-
-    public constructor (opts: iMediatorUserOptions) {
-
-        super(opts);
-
-        this._onCommandRunning = this._onCommandRunning.bind(this);
-        this._onCommandSuccess = this._onCommandSuccess.bind(this);
-        this._onCommandFail = this._onCommandFail.bind(this);
-
-    }
 
     public _initWorkSpace (): Promise<void> {
 
@@ -50,15 +37,15 @@ export default class ServerStreamDeck extends Server {
 
     // events
 
-    private _onCommandRunning (command: components["schemas"]["Command"]): void {
+    private readonly _onCommandRunning = (command: components["schemas"]["Command"]): void => {
 
         const event: components["schemas"]["PushEventCommandRunning"]["data"] = command;
 
         this.push("command.running", event);
 
-    }
+    };
 
-    private _onCommandSuccess (command: components["schemas"]["Command"], content: string): void {
+    private readonly _onCommandSuccess = (command: components["schemas"]["Command"], content: string): void => {
 
         const event: components["schemas"]["PushEventCommandSuccess"]["data"] = {
             "content": content,
@@ -67,9 +54,9 @@ export default class ServerStreamDeck extends Server {
 
         this.push("command.success", event);
 
-    }
+    };
 
-    private _onCommandFail (command: components["schemas"]["Command"], err: components["schemas"]["Error"]): void {
+    private readonly _onCommandFail = (command: components["schemas"]["Command"], err: components["schemas"]["Error"]): void => {
 
         const event: components["schemas"]["PushEventCommandFail"]["data"] = {
             "command": command,
@@ -78,6 +65,6 @@ export default class ServerStreamDeck extends Server {
 
         this.push("command.fail", event);
 
-    }
+    };
 
 }
