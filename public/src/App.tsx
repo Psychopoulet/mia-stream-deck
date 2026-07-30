0db4e0b15f17928a043e73ef4b6d1424e818bd60
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-07-30 20:39:08 +0200

    remove inputs from API

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -59,9 +59,10 @@ export default class App extends React.Component<iPropsNode, iState> {
             return;
         }
 
-        this._sdk.addEventListener("connected", this._onConnected);
-        this._sdk.addEventListener("disconnected", this._onDisconnected);
-        this._sdk.addEventListener("error", this._onError);
+        this._sdk
+            .on("connected", this._onConnected)
+            .on("disconnected", this._onDisconnected)
+            .on("error", this._onError);
 
         this._sdk.connect();
 
@@ -69,9 +70,10 @@ export default class App extends React.Component<iPropsNode, iState> {
 
     public componentWillUnmount (): void {
 
-        this._sdk.removeEventListener("connected", this._onConnected);
-        this._sdk.removeEventListener("disconnected", this._onDisconnected);
-        this._sdk.removeEventListener("error", this._onError);
+        this._sdk
+            .off("connected", this._onConnected)
+            .off("disconnected", this._onDisconnected)
+            .off("error", this._onError);
 
         this._sdk.disconnect();
 
@@ -112,10 +114,10 @@ export default class App extends React.Component<iPropsNode, iState> {
 
     };
 
-    private readonly _onError = (event: CustomEvent<components["schemas"]["PushEventPluginError"]["data"]>): void => {
+    private readonly _onError = (err: components["schemas"]["PushEventPluginError"]["data"]): void => {
 
         this.setState({
-            "error": event.detail
+            "error": err
         });
 
     };
